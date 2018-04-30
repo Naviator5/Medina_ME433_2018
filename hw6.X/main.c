802d1971e4e54a2602f92df3ab6a961c6c7671d1
Xavier Medina <XavierMedina2018@u.northwestern.edu>
2018-04-30 16:50:35 -0500

    HW6 C2
    finished code

--- hw6.X/main.c
+++ hw6.X/main.c
@@ -43,8 +43,9 @@
 
 /* Function Prototypes */
 void drawString(unsigned short, unsigned short, char *, unsigned short, unsigned short);
-void drawChar(unsigned short, unsigned short, char, unsigned short, unsigned short);
-void drawProgressBar(unsigned short, unsigned short, unsigned short, unsigned short, unsigned short, unsigned short, unsigned short);
+void drawChar(unsigned short, unsigned short, char, unsigned short, unsigned short); 
+void drawProgressBar(unsigned short, unsigned short, unsigned short, \
+ unsigned short, unsigned short, unsigned short, unsigned short);
 
 
 /* Main Function */
@@ -59,46 +60,85 @@ int main(void) {
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
     
-    LCD_init();
+    LCD_init(); // initialize LCD/SPI communication
     __builtin_enable_interrupts();
     
-    LCD_clearScreen(BLACK);
+    LCD_clearScreen(BLACK); // clear the screen to background color of choice
+    
+    int i = 0;
     while(1) {
+        _CP0_SET_COUNT(0);
         char hello[30];
+        char framerate[20];
+      
+        int fps_start = _CP0_GET_COUNT();  // timer for 1 frame to display 
+        sprintf(hello, "Hello World! %d   ",i);
+        drawString(28, 32, hello, WHITE, BLACK); 
+        drawProgressBar(14, 45, 4, i, BLUE, 100-i, WHITE);
         
-        sprintf(hello, "Hello World!");
-        drawString(10, 10, hello, WHITE, BLACK);
+        // calculate frames per second and display on screen 
+        float fps = 24000000.0/(_CP0_GET_COUNT() - fps_start);
+        sprintf(framerate, "FPS = %3.2f",fps);
+        drawString(14, 60, framerate, WHITE, BLACK); 
+        
+        // update counter/progress bar
+        i++;
+        if (i == 100) {i = 0;}
+                
+        while(_CP0_GET_COUNT() < 2400000) {Nop();} // update speed = 10 Hz
     }
     return 0;
 }
 
 
 /* Helper Functions */
-void drawChar(unsigned short x, unsigned short y, char message, unsigned short color1, unsigned short color2) {
-    /* set variables for rows and columns of ASCII array */
-    char row = message - 0x20;
+void drawChar(unsigned short x, unsigned short y, char message, \
+ unsigned short color1, unsigned short color2) { 
+    char row = message - 0x20; // variables for rows and columns of ASCII array
     int col = 0;
     
-    /* loop through array */
+    // loop through array 
     for(col = 0; col < 5; col++) {
         char pixels = ASCII[row][col];
         int j = 0;
         
         for(j = 0; j < 8; j++) {
             if ((x+col) < 128 && (y+j) < 160) {    // condition to remain on 128x160 screen
-                if ( ((pixels >> j) & 1) == 1) {
+                if ( ((pixels >> j) & 1) == 1) {   // if = 1, fill in the pixel w character color
                 LCD_drawPixel(x+col, y+j, color1);
-                } else {LCD_drawPixel(x+col, y+j, color2);}
+                } else {LCD_drawPixel(x+col, y+j, color2);} // otherwise leave as background color
             }
         }
     }
 }
 
-void drawString(unsigned short x, unsigned short y, char *message, unsigned short color1, unsigned short color2) {
+void drawString(unsigned short x, unsigned short y, char *message, \
+ unsigned short color1, unsigned short color2) {
     int i = 0;
     
     while (message[i]) {
         drawChar(x+(i*5), y, message[i], color1, color2);
         i++;
     }
+}
+
+void drawProgressBar(unsigned short x, unsigned short y, unsigned short h, \
+ unsigned short len1, unsigned short color1, unsigned short len2, unsigned short color2) {
+    // make background for bar (across, then down)
+    int i = 0, j = 0;
+    
+    // this part controls the color that fills the bar (when i = 0, bar is all background)
+    for (j = 0; j <= h; j++) { 
+        for (i = 0; i <= len1; i++) {
+            LCD_drawPixel(x+i,y+j,color1);
+        }
+    }
+    
+    // this part controls the background color
+    for (j = 0; j <= h; j++) {
+        for (i = 0; i <= len2; i++) {
+            LCD_drawPixel((x+100)-i,y+j,color2); 
+        }
+    }      
+    
 }
